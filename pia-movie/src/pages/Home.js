b85d25f845b760351c00b75a762582d6e905bd41
NidiaGzz <nidiav.gc@hotmail.com>
2022-10-27 15:56:08 -0500

    adios slider u.u
    tmb adios bootstrap kk

--- pia-movie/src/pages/Home.js
+++ pia-movie/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CarouselContainer from "../components/CarouselContainer";
+
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home(){
     return(
         <div>
         <Navbar/>
-        <CarouselContainer/>
+        
         HOME PERRA!!!
         </div>
     )
