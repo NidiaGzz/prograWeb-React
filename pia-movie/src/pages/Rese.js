879dd2e51484c44ac233726331973d20d52102fc
NidiaGzz <nidiav.gc@hotmail.com>
2022-10-28 01:21:53 -0500

    2do avance???
    Co-Authored-By: KaryMaly <78707738+KaryMaly@users.noreply.github.com>

--- pia-movie/src/pages/Rese.js
+++ pia-movie/src/pages/Rese.js
@@ -1,67 +1,89 @@
-import { Button } from '@mui/material';
-import React, { useState } from 'react'
-import '../components/Card.css'
 import Navbar from "../components/Navbar/Navbar";
 import styles from './Catalogo.module.css';
+import React from "react";
+import { useState } from "react";
+import './ReseStyle.css';
 
-export const Card = () => {
 
-    const [name, setName] = useState('Nombre');
-    const [user, setUser] = useState('usuario');
-    const [email, setEmail] = useState('correo');
+export default function Rese() {
+    const [cards] = useState([
+        {
+            title: 'User1',
+            movie: 'Movie Name',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        },
+        {
+            title: 'User2',
+            movie: 'Movie Name',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        },
+        {
+            title: 'User3',
+            movie: 'Movie Name',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        },
+        {
+            title: 'User4',
+            movie: 'Movie Name',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        },
+        {
+            title: 'User5',
+            movie: 'Movie Name',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        },
+        {
+            title: 'User6',
+            movie: 'Movie Name',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        },
+
+    ]);
 
     return (
         <div>
-            <header>
-                <Navbar />
-                <h1 className={styles.title}>RESEÑAS</h1>
-            </header>
-            <div className={styles.resenas}>
-                <div className='contenedor'>
-                    <div className='cont-cards'>
-                        <div className='Card2'>
-
-                            <h1>PELICULA</h1>
-                            <h4>Persona</h4>
-                            <h4>blablablabkla bla bla blabla</h4>
-
-                        </div>
-                        <div className='Card2'>
-
-                            <h1>PELICULA</h1>
-                            <h2>Persona</h2>
-                            <h4>blablablabkla bla bla blabla</h4>
-
-                        </div>
-                        <div className='Card2'>
-                            <h1>PELICULA</h1>
-                            <h2>Persona</h2>
-                            <h4>blablablabkla bla bla blabla</h4>
-
-                        </div>
-                        <div className='Card2'>
-
-                            <h1>PELICULA</h1>
-                            <h2>Persona</h2>
-                            <h4>blablablabkla bla bla blabla</h4>
-
-                        </div>
-
-                        <div className='Card2'>
-
-                            <h1>PELICULA</h1>
-                            <h2>Persona</h2>
-                            <h4>blablablabkla bla bla blabla</h4>
-
-                        </div>
-
+            <Navbar />
+            <h1 className={styles.title}>RESEÑAS</h1>
+            <section>
+                <div className="container">
+                    <div className="cards">
+                        {
+                            cards.map((card, i) => (
+                                <div key={i} className="card">
+                                    <h1>{card.title}</h1>
+                                    <h4>{card.movie}</h4>
+                                    <p>{card.text}</p>
+                                    <button className="btn">See more</button>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
-
-            </div>
-
+            </section>
         </div>
     )
 }
-
-export default Card
